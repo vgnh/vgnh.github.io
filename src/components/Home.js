f6fd379b9abbce3831accde0a333f363f8c2585a
Vignesh Mani <vigneshm21@protonmail.com>
2022-01-17 17:17:53 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,16 +13,13 @@ const Home = () => {
         <p class="para_about">
           Hi there! I'm Vignesh.
           <br /><br />
-          I'm a developer who currently works with backend web technologies such as Node.js (Express).
-          Other technologies I've worked with in the past include Java, Python, and a bit of C# and Kotlin.
-          I've also worked a little with frontend technologies such as HTML/CSS/JavaScript and React in the past.
-          I enjoy working with open source software, learning new technologies and languages.
+          I'm currently a backend developer working with technologies such as Node.js, primarily on MERN-stack based projects.
+          Other technologies I've worked with in the past include Java, Python, and a bit of HTML/CSS and React.
+          I enjoy working with open source software (a Fedora Linux user at home as well), learning new technologies and languages.
           <br /><br />
-          I'm fairly new to the web stack and I'm still learning the design principles.<br />
-          While this may be the case, this is the stack I've chosen to become proficient at as of now.<br />
           I have varied interests when it comes to software, and in my free time you can often find me reading/learning about things
           such as retro game programming books, IoT, retro console emulators, etc. My current interests include learning and becoming proficient at a
-          systems programming language such as Rust and a general purpose language such as Java.
+          systems programming language such as Rust. (and consequently have a career in building various applications with it)
           <br /><br />
           My other unrelated interests include learning to read and speak 日本語, which I must say is currently on pause.<br />
           I'm also a huge <a href="https://en.wikipedia.org/wiki/Gundam" target="_blank" rel="noreferrer">Gundam</a> fan and I'd even attribute it to laying the foundation for my interest in Computer Science back when I was a little kid.
@@ -32,7 +29,7 @@ const Home = () => {
           You can also download my <a href="https://github.com/vgnh/vgnh.github.io/releases/download/0.1.0/Vignesh-Mani-Resume.pdf">Resume</a>.
         </p>
         <p class="para_last_updated">
-          Last updated: 24-Sep-2021
+          Last updated: 17-Jan-2022
         </p>
       </div>
     </div>
