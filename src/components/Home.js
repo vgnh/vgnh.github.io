fcc27f97cf238f11a664241254ee1ec6fdd37d91
Vignesh Mani <vignesh@kawa.space>
2022-07-08 11:30:40 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,23 +13,19 @@ const Home = () => {
         <p class="para_about">
           Hi there! I'm Vignesh.
           <br /><br />
-          I'm currently a backend developer working with technologies such as Node.js, primarily on MERN-stack based projects.
+          I'm currently a backend developer working with technologies such as Go, Docker, Kubernetes, primarily on the GCP-stack.
           Other technologies I've worked with in the past include Java, Python, and a bit of HTML/CSS and React.
           I enjoy working with open source software (a Fedora Linux user at home as well), learning new technologies and languages.
           <br /><br />
           I have varied interests when it comes to software, and in my free time you can often find me reading/learning about things
           such as retro game programming books, IoT, retro console emulators, etc. My current interests include learning and becoming proficient at a
-          systems programming language such as Rust. (and consequently have a career in building various applications with it)
+          systems programming language such as Go/Rust. (and consequently have a career in building various applications with it)
           <br /><br />
           My other unrelated interests include learning to read and speak 日本語, which I must say is currently on pause.<br />
-          I'm also a huge <a href="https://en.wikipedia.org/wiki/Gundam" target="_blank" rel="noreferrer">Gundam</a> fan and I'd even attribute it to laying the foundation for my interest in Computer Science back when I was a little kid.
           <br /><br />
           You can find me on <a href="https://github.com/vgnh" target="_blank" rel="noreferrer">GitHub</a> and <a href="https://twitter.com/vigneshm_" target="_blank" rel="noreferrer">Twitter</a>.<br />
           Business enquiry? Send me an <a href="mailto:vigneshofficial03@gmail.com">Email</a>.<br />
-          You can also download my <a href="https://github.com/vgnh/vgnh.github.io/releases/download/0.1.0/Vignesh-Mani-Resume.pdf">Resume</a>.
-        </p>
-        <p class="para_last_updated">
-          Last updated: 17-Jan-2022
+          You can also download my <a href="https://github.com/vgnh/vgnh.github.io/releases/download/0.1.0/Vignesh-Mani-Resume.pdf">Resume</a>. (Last updated: 01-July-2022)
         </p>
       </div>
     </div>
